74902362f3aa02d8b43acbadedb8762d88248483
Victor Sz <vicsz@users.noreply.github.com>
2024-11-27 13:58:35 -0500

    Don't forget master password

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -3,3 +3,9 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+
+variable "master_password" {
+  description = "Master password for the database"
+  type        = string
+  sensitive   = true
+}
