08a8c41c6cf4f4218664239fd03d3a2e07b3779c
Victor Sz <vicsz@users.noreply.github.com>
2024-11-27 14:07:20 -0500

    Add prod config.

--- terraform/environments/prod/terraform.tfvars
+++ terraform/environments/prod/terraform.tfvars
@@ -1,1 +1,4 @@
 environment = "prod"
+aws_region  = "us-east-1"
+
+# Note: The master_password is expected to be injected at runtime, such as through GitHub Secrets or another secure method.
--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -1,3 +1,17 @@
-terraform {
-  backend "s3" {}
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "master_password" {
+  description = "Master password for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
 }
