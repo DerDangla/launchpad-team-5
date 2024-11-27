1780913d75ae34fda667751ed3e62ec66ee2d2b4
Victor Sz <vicsz@users.noreply.github.com>
2024-11-27 14:00:04 -0500

    Fix vpc issues.

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,3 +1,32 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = var.name
+  }
 }
+
+resource "aws_subnet" "private" {
+  count             = var.private_subnet_count
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "${var.name}-private-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = var.public_subnet_count
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.name}-public-subnet-${count.index + 1}"
+  }
+}
+
+data "aws_availability_zones" "available" {}
--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,8 +1,3 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.main.id
-}
-
 output "private_subnet_ids" {
   description = "IDs of private subnets"
   value       = aws_subnet.private[*].id
